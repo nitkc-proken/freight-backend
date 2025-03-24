2cc76c663c985ab372dcaa416a9fb6dc3ae22db7
Naotiki <naotiki@naotiki.me>
2025-03-25 00:43:12 +0900

    refactor: add EntityRepository to impl some method automatically

--- src/main/kotlin/io/github/nitkc_proken/freight/backend/repository/Repository.kt
+++ src/main/kotlin/io/github/nitkc_proken/freight/backend/repository/Repository.kt
@@ -1,9 +1,40 @@
 package io.github.nitkc_proken.freight.backend.repository
 
+import io.github.nitkc_proken.freight.backend.utils.suspendTransaction
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.EntityClass
+
 interface Repository<T : Model<ID>, ID : Any> {
     suspend fun list(): List<T>
     suspend fun get(id: ID): T?
-    suspend fun create(t: T): T
-    suspend fun update(t: T): T
+    suspend fun create(value: T): T
+    suspend fun update(value: T): T
     suspend fun delete(id: ID)
+}
+
+// 自動実装
+abstract class EntityRepository<T : Model<ID>, ID : Any, E : Entity<ID>, EC : EntityClass<ID, E>>(
+    private val entityClass: EC,
+    private val entityToModel: EntityToModel<E, T>
+) : Repository<T, ID> {
+    override suspend fun list(): List<T> = suspendTransaction {
+        with(entityToModel) {
+            entityClass.all().toModels()
+        }
+    }
+
+    override suspend fun get(id: ID): T? = suspendTransaction {
+        with(entityToModel) {
+            entityClass.findById(id)?.toModel()
+        }
+    }
+
+    override suspend fun delete(id: ID) = suspendTransaction {
+        entityClass[id].delete()
+    }
+
+    abstract override suspend fun create(value: T): T
+
+    abstract override suspend fun update(value: T): T
+
 }
--- src/main/kotlin/io/github/nitkc_proken/freight/backend/repository/impl/ContainerRepositoryImpl.kt
+++ src/main/kotlin/io/github/nitkc_proken/freight/backend/repository/impl/ContainerRepositoryImpl.kt
@@ -4,90 +4,37 @@ import io.github.nitkc_proken.freight.backend.entity.ContainerEntity
 import io.github.nitkc_proken.freight.backend.entity.NetworkEntity
 import io.github.nitkc_proken.freight.backend.repository.Container
 import io.github.nitkc_proken.freight.backend.repository.Container.Companion.toModel
-import io.github.nitkc_proken.freight.backend.repository.Container.Companion.toModels
 import io.github.nitkc_proken.freight.backend.repository.ContainerRepository
+import io.github.nitkc_proken.freight.backend.repository.EntityRepository
 import io.github.nitkc_proken.freight.backend.utils.suspendTransaction
 import org.koin.core.annotation.Single
 import kotlin.uuid.Uuid
 
 @Single
-class ContainerRepositoryImpl : ContainerRepository {
+class ContainerRepositoryImpl : ContainerRepository,
+    EntityRepository<Container, Uuid, ContainerEntity, ContainerEntity.Companion>
+        (ContainerEntity, Container) {
 
-    override suspend fun list(): List<Container> = suspendTransaction {
-        ContainerEntity.all().toModels()
-    }
-
-    override suspend fun get(id: Uuid): Container? = suspendTransaction {
-        ContainerEntity.findById(id)?.toModel()
-    }
-
-    override suspend fun create(t: Container): Container = suspendTransaction {
-        ContainerEntity.new(t.id) {
-            this.containerId = t.containerId
-            this.network = NetworkEntity[t.network.toEntityId()]
-            this.ipAddress = t.ipAddress
-            this.hostVEthName = t.hostVEthName
-            this.containerVEthName = t.containerVEthName
+    override suspend fun create(value: Container): Container = suspendTransaction {
+        ContainerEntity.new(value.id) {
+            containerId = value.containerId
+            network = NetworkEntity[value.network.toEntityId()]
+            ipAddress = value.ipAddress
+            hostVEthName = value.hostVEthName
+            containerVEthName = value.containerVEthName
+            shortId = value.shortId
         }.toModel()
     }
 
-    override suspend fun update(t: Container): Container = suspendTransaction {
-        ContainerEntity[t.id].apply {
-            containerId = t.containerId
-            network = NetworkEntity[t.network.toEntityId()]
-            ipAddress = t.ipAddress
-            hostVEthName = t.hostVEthName
-            containerVEthName = t.containerVEthName
+    override suspend fun update(value: Container): Container = suspendTransaction {
+        ContainerEntity[value.id].apply {
+            containerId = value.containerId
+            network = NetworkEntity[value.network.toEntityId()]
+            ipAddress = value.ipAddress
+            hostVEthName = value.hostVEthName
+            containerVEthName = value.containerVEthName
+            shortId = value.shortId
         }.toModel()
     }
 
-    override suspend fun delete(id: Uuid): Unit = suspendTransaction {
-        ContainerEntity[id].delete()
-    }
-
-    /*override suspend fun updateNICNames(
-        containerEntity: ContainerEntity,
-        vEthNetworkInterfaceName: NetworkInterfaceName,
-        vEthContainerInterfaceName: NetworkInterfaceName,
-    ): ContainerEntity = suspendTransaction {
-        containerEntity.apply {
-            this.hostVEthName = vEthNetworkInterfaceName
-            this.containerVEthName = vEthContainerInterfaceName
-        }
-    }
-
-    override suspend fun createContainer(networkId: EntityID<Uuid>, ipAddress: IPv4Address): Container? =
-        suspendTransaction {
-            val container = dockerClient.createContainerCmd(CONTAINER_IMAGE)
-                .withHostConfig(DefaultHostConfig).withTty(true).withAttachStdin(true).exec()
-            val networkEntity = NetworkEntity[networkId]
-            val containerEntity = ContainerEntity.new {
-                this.containerId = DockerId(container.id)
-                this.network = networkEntity
-                this.ipAddress = ipAddress
-            }
-            commit()
-            val containerModel = containerEntity.toModel()
-
-            dockerClient.startContainerCmd(container.id).exec()
-            val networkModel = networkEntity.toModel()
-            val nsPath = dockerClient.inspectContainerCmd(container.id).exec().networkSettings.sandboxKey
-            gatewayClient.initiateContainerNetwork(
-                networkModel,
-                networkModel.bridgeInterfaceName!!,
-                containerModel.vEthHostInterfaceNameCandidate,
-                containerModel.vEthContainerInterfaceNameCandidate,
-                nsPath.toString(),
-                ipAddress
-            )
-            containerEntity.apply {
-                this.hostVEthName = containerModel.vEthHostInterfaceNameCandidate
-                this.containerVEthName = containerModel.vEthContainerInterfaceNameCandidate
-            }.toModel()
-            /*updateNICNames(
-                containerEntity,
-                networkModel.vEthInterfaceNameCandidate,
-                networkModel.vEthContainerInterfaceNameCandidate
-            )*/
-        }*/
 }
